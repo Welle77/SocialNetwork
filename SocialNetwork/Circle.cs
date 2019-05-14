53cf01df9cb609c9f7a39b6fc9128cba4bbee400
Jesper <201707587@post.au.dk>
2019-05-14 13:01:28 +0200

    Something

---
+++ SocialNetwork/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialNetwork.Models
+{
+    public class Circle
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Members")]
+        public List<User> Members { get; set; }
+
+    }
+}
---
+++ SocialNetwork/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialNetwork.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("BlockedList")]
+        public List<User> BlockedList { get; set; }
+
+        [BsonElement("Circles")]
+        public List<Circle> Circles { get; set; }
+
+        [BsonElement("Friends")]
+        public List<User> Friends { get; set; }
+    }
+}
