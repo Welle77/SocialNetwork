89c73d157742ad284ab8f05896e82c385eec777c
Christian Wraa Melsen <christian@melsen.info>
2019-05-14 19:32:00 +0200

    Added support for Comments on Posts now, also removed CommentService since there will be no collection of comments

--- SocialNetwork/Services/PostService.cs
+++ SocialNetwork/Services/PostService.cs
@@ -39,6 +39,38 @@ namespace SocialNetwork.Services
             _posts.InsertOne(postToBeAdded);
         }
 
+        public void PostComment(string postId, Comment commentToBeAdded)
+        {
+            //Takes the first element because the should only ever be one (PostId is unique)
+            try
+            {
+                PostComment((_posts.Find(p => p.Id == postId).ToList())[0], commentToBeAdded);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something bad happened (Post might not exist)");
+            }
+        }
+
+        public void PostComment(Post post, Comment commentToBeAdded)
+        {
+            //No concurrency safety (Should probably use some sort of Optimistic Concurrency framework instead)
+            try
+            {
+                postComment(post, commentToBeAdded);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something bad happened (Post might not exist)");
+            }
+        }
+
+        private void postComment(Post post, Comment commentToBeAdded)
+        {
+            post.Comments.Add(commentToBeAdded);
+            var result = _posts.ReplaceOne(p => p.Id == post.Id, post);
+        }
+
         public List<Post> GetPostsByAuthorId(string Id)
         {
             return _posts.Find(p => p.Author.Id == Id).ToList();
