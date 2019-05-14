323ae3ea101261bb2bc770d4dc9633573b262269
Christian Wraa Melsen <christian@melsen.info>
2019-05-14 18:06:07 +0200

    Modelled Post.cs and populated PostService.cs. Also made a CreateIndexes() in PostService to optimize the searches it provides for regular use.

--- SocialNetwork/Models/Post.cs
+++ SocialNetwork/Models/Post.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace SocialNetwork.Models
 {
-    class Post
+    public class Post
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Author")]
+        public User Author { get; set; }
+
+        [BsonElement("Content")]
+        public string Content { get; set; }
+
+        [BsonElement("Circle")]
+        public Circle AssociatedCircle { get; set; }
+
+        [BsonElement("Comments")]
+        public List<Comment> Comments { get; set; }
     }
 }
--- SocialNetwork/Services/PostService.cs
+++ SocialNetwork/Services/PostService.cs
@@ -17,5 +17,36 @@ namespace SocialNetwork.Services
         {
             _posts = Client.GetCollection<Post>("Posts");
         }
+
+        public void CreateIndexes()
+        {
+            //Source https://stackoverflow.com/questions/51248295/mongodb-c-sharp-driver-create-index
+            //Answer by: StuiterSlurf
+
+            //Index on authors userid
+            var indexKeysAuthor = Builders<Post>.IndexKeys;
+            var indexModelAuthor = new CreateIndexModel<Post>(indexKeysAuthor.Ascending(p => p.Author.Id));
+            _posts.Indexes.CreateOne(indexModelAuthor);
+
+            //Index on associatedCircles CircleId
+            var indexKeysCircle = Builders<Post>.IndexKeys;
+            var indexModelCircle = new CreateIndexModel<Post>(indexKeysCircle.Ascending(p => p.AssociatedCircle.Id));
+            _posts.Indexes.CreateOne(indexModelCircle);
+        }
+
+        public void AddPost(Post postToBeAdded)
+        {
+            _posts.InsertOne(postToBeAdded);
+        }
+
+        public List<Post> GetPostsByAuthorId(string Id)
+        {
+            return _posts.Find(p => p.Author.Id == Id).ToList();
+        }
+
+        public List<Post> GetPostsByCircleId(string Id)
+        {
+            return _posts.Find(p => p.AssociatedCircle.Id == Id).ToList();
+        }
     }
 }
