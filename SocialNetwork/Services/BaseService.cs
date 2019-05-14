2eff7af4c90ce34af1277b8f8be76ad6d6adf793
Jesper <201707587@post.au.dk>
2019-05-14 12:13:41 +0200

    Added models and a baseservice, all services should inherit from this and call base in the ctor

---
+++ SocialNetwork/Services/BaseService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace SocialNetwork.Services
+{
+    public abstract class BaseService
+    {
+        private readonly IMongoDatabase _client;
+
+        public BaseService()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("BookstoreDb");
+            _client = database;
+        }
+    }
+}
